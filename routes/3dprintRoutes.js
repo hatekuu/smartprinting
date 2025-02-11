1261b65f62c42b1f805f71e6b4af3406cbb5e20b
unknown <hatekuu@gmail.com>
2025-02-11 18:57:00 +0700

    dong123123

--- routes/3dprintRoutes.js
+++ routes/3dprintRoutes.js
@@ -1,20 +1,22 @@
 const express = require('express');
-const { uploadFile,getCommandAndUpdateStatus,uploadGcodeFile,sendCommand,getPrinter,updateStatus,uploadStlChunk,confirmOrder,processGcodePricing,downloadStl,confirmDownload} = require( '../controllers/3dprintController');
+const { uploadFile, getCommandAndUpdateStatus, uploadGcodeFile, sendCommand, getPrinter, updateStatus, uploadStlChunk, confirmOrder, processGcodePricing, downloadStl, confirmDownload } = require('../controllers/3dprintController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const multer = require("multer");
 const router = express.Router();
-// Cấu hình Multer để lưu file tạm
-const upload = multer({ dest: "uploads/" });
+
+// Cấu hình Multer để lưu file tạm vào thư mục /tmp/uploads trên Vercel
+const upload = multer({ dest: '/tmp/uploads/' });
+
 router.post('/getCommand', getCommandAndUpdateStatus);
 router.post('/uploadFile', uploadGcodeFile);
 router.post('/sendCommand', sendCommand);
 router.post('/updateStatus', updateStatus);
-router.post('/getPrinter',authMiddleware,getPrinter)
-router.post('/uploadStl',authMiddleware,uploadStlChunk)
+router.post('/getPrinter', authMiddleware, getPrinter);
+router.post('/uploadStl', authMiddleware, uploadStlChunk);
 router.post('/confirm-order', confirmOrder);
 router.post('/gcodepricing', processGcodePricing);
 router.post('/download-stl', downloadStl);
 router.post('/confirm-download', confirmDownload);
-router.post("/upload", upload.single("file"),uploadFile);
+router.post("/upload", upload.single("file"), uploadFile);
 
 module.exports = router;
